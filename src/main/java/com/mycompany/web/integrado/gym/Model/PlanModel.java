b18fa070bb8392296c9f89e8c3ce0d14e1976439
elmap <elmapirp@gmail.com>
2025-09-07 19:45:59 -0800

    agregue

--- src/main/java/com/mycompany/web/integrado/gym/Model/PlanModel.java
+++ src/main/java/com/mycompany/web/integrado/gym/Model/PlanModel.java
@@ -1,8 +1,79 @@
 package com.mycompany.web.integrado.gym.Model;
 
-public class  PlanModel {
 
+public class PlanModel {
 
+    private int id_plan, duracion_dias;
+    private String nombre, descripcion, tipo, estado;
+    private double precio;
 
+    public PlanModel() {
+    }
+
+    public PlanModel(int id_plan, int duracion_dias, String nombre, String descripcion, String tipo, String estado, double precio) {
+        this.id_plan = id_plan;
+        this.duracion_dias = duracion_dias;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.tipo = tipo;
+        this.estado = estado;
+        this.precio = precio;
+    }
+
+    public int getId_plan() {
+        return id_plan;
+    }
+
+    public void setId_plan(int id_plan) {
+        this.id_plan = id_plan;
+    }
+
+    public int getDuracion_dias() {
+        return duracion_dias;
+    }
+
+    public void setDuracion_dias(int duracion_dias) {
+        this.duracion_dias = duracion_dias;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 
 }
